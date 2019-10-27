6e0f33428a9c88098f4ee8309a18d869ad941ee4
maksthorn <maksdrzezdzon@gmail.com>
2019-10-27 18:10:27 +0000

    swift recap

--- swift/recapOnSwift.swift
+++ swift/recapOnSwift.swift
@@ -1,19 +1,25 @@
 //main link + notes https://www.tutorialspoint.com/swift/index.htm
+//array link https://developer.apple.com/documentation/swift/array
+
+/*****************************************************************/
+/*****************************************************************/
+import cocoa
+//imports can be any objective c, swift or c library
 // variables
-var str = "this is a test"
-let constant = 12
-var number:Int = 23
-var decimal:Float = 2.1
+var str = "this is a test";
+let constant = 12;
+var number:Int = 23;
+var decimal:Float = 2.1;
+// ';' are not manditory in swift but they are allowed
 
-print("This is a string \(str)  this is a constant \(constant)  this is a number \(number)  this is a decimal  \(decimal)")
-var allow:Bool = true
-var close:Bool = false
-var numbers: [Float] = [Float(constant), Float(number), decimal]
+print("This is a string \(str)  this is a constant \(constant)  this is a number \(number)  this is a decimal  \(decimal)");
+var allow:Bool = true;
+var close:Bool = false;
+var numbers: [Float] = [Float(constant), Float(number), decimal];
 
-var strWithType: String = "test string"
-let constantWithType: String = " "
+var strWithType: String = "test string";
+let constantWithType: String = " ";
 
-// array link https://developer.apple.com/documentation/swift/array
 
 //functions
 
@@ -21,4 +27,39 @@ func function(data: String) -> String{
 	//similar to how python in web.
 	return data
 }
-print(function(data: strWithType))
+print(function(data: strWithType));
+
+/*
+  _                                    _     
+ | |                                  | |    
+ | | _____ _   ___      _____  _ __ __| |___ 
+ | |/ / _ \ | | \ \ /\ / / _ \| '__/ _` / __|
+ |   <  __/ |_| |\ V  V / (_) | | | (_| \__ \
+ |_|\_\___|\__, | \_/\_/ \___/|_|  \__,_|___/
+            __/ |                            
+           |___/                             
+
+Class	deinit	Enum	extension
+Func	import	Init	internal
+Let	operator	private	protocol
+public	static	struct	subscript
+typealias	var
+
+
+// key in statements
+
+break	case	continue	default
+do	else	fallthrough	for
+if	in	return	switch
+where	while
+
+//key in contexts
+
+associativity	convenience	dynamic	didSet
+final	get	infix	inout
+lazy	left	mutating	none
+nonmutating	optional	override	postfix
+precedence	prefix	Protocol	required
+right	set	Type	unowned
+weak	willSet
+*/
---
+++ swift/swiftSyntax.swift
@@ -0,0 +1,3 @@
+print("items to print are ", item:"value", key:"value")
+print("5+3 is \(5+3)");
+
