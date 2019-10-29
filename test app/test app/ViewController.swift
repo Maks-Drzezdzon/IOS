1ecc9751676eaba7cf9e0b6f9c385198ede94c14
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-10-29 20:46:19 +0000

    got my mac and degree today woop

---
+++ test app/test app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test app
+//
+//  Created by Maks Drzezdzon on 29/10/2019.
+//  Copyright © 2019 Maks Drzezdzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
