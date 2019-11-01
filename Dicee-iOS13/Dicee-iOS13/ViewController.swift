866d620954d695185bd452f69349a9489c355021
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-01 18:27:31 +0000

    back on ios woop

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftDiceView: UIImageView!
+    @IBOutlet weak var rightDiceView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // who.what = value
+        leftDiceView.image = #imageLiteral(resourceName: "DiceSix")
+        rightDiceView.image = #imageLiteral(resourceName: "DiceTwo")
+
     }
 
 
