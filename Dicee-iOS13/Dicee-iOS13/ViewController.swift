98a1081e6703b47abeba4bc9427d6cfe232add52
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-01 19:51:29 +0000

    playing around with methods

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -5,11 +5,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftDiceView: UIImageView!
     @IBOutlet weak var rightDiceView: UIImageView!
     
-    var diceImagesArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    let diceImagesArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     @IBAction func rollButton(_ sender: UIButton) {
         let  randomRoll = Int.random(in: 0...5)
-        leftDiceView.image = diceImagesArray [randomRoll]
+        leftDiceView.image = diceImagesArray.randomElement()
         rightDiceView.image = diceImagesArray [randomRoll]
 
     }
