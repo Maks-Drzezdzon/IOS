14e250cff0f2039d35176922b9d9f7cd802a4e04
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-04 20:12:47 +0000

    timer fix

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     let eggTimes = ["Soft":300, "Hard":720, "Medium":420]
     var secondsLeft = 60
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        // resets/creates timer when called/pressed
         let hardness = sender.currentTitle!
         // ! removes safety check
         let result = eggTimes[hardness]!
         
         Timer.scheduledTimer(timeInterval: 1.0,target: self,selector:#selector(updateTimer),userInfo:nil, repeats: true)
     }
-    
+    // selector comes from obj c
     @objc func updateTimer(){
         if secondsLeft > 0{
             print("\(secondsLeft) seconds")
