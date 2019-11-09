147932fb8cd1bbbc3ac76726f76acd965defe2c0
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-09 18:19:23 +0000

    adding multiple answer options to mvc version

--- Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionText: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var answerButton1: UIButton!
+    @IBOutlet weak var answerButton2: UIButton!
+    @IBOutlet weak var answerButton3: UIButton!
+    
     
     // replace later with api for random questions etc
     var quizData = QuizData()
@@ -44,8 +46,11 @@ class ViewController: UIViewController {
         progressBar.progress = quizData.getProgress()
         
         scoreLabel.text = "Score \(quizData.getScore())"
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+        
+        answerButton1.backgroundColor = UIColor.clear
+        answerButton2.backgroundColor = UIColor.clear
+        answerButton3.backgroundColor =
+        UIColor.clear
         // add 1 to start with some prog and have a full bar later
         
     }
