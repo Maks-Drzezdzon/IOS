c230c9c24aaa6c16a85002c52637b90c9b1b1f8a
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-10 16:32:35 +0000

    starting on BMI App

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hLabel: UILabel!
+    @IBOutlet weak var wLabel: UILabel!
+    
+    @IBAction func hSlider(_ sender: UISlider) {
+        hLabel.text = String(format: "%.2f", sender.value)
+    }
+    
+    @IBAction func wSlider(_ sender: UISlider) {
+        wLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
