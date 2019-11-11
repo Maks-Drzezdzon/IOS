7c7d34a23985d353ed28c8b4a906b90da5a16603
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-11 18:09:00 +0000

    setting up facebook project

---
+++ FacebookUIClone/FacebookUIClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookUIClone
+//
+//  Created by Maks Drzezdzon on 11/11/2019.
+//  Copyright © 2019 Maks Drzezdzon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacebookUIClone/FacebookUICloneTests/FacebookUICloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  FacebookUICloneTests.swift
+//  FacebookUICloneTests
+//
+//  Created by Maks Drzezdzon on 11/11/2019.
+//  Copyright © 2019 Maks Drzezdzon. All rights reserved.
+//
+
+import XCTest
+@testable import FacebookUIClone
+
+class FacebookUICloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
