7ee28fd4376d68d4f26e31bae22e5653bf9836cb
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-12 18:28:27 +0000

    added syntax

--- swift/swift classes/swift classes/conditionals.swift
+++ swift/swift classes/swift classes/conditionals.swift
@@ -1,26 +1,30 @@
-var name = "maks"
-//if stmt
+class conditionals {
+    
+    var name = "maks"
+    //if stmt
 
-if name != nil {
-	print("do things")
-}else if name == 10{
-	print("do other things")
-}else{
-	print(name)
-}
+    if name != nil {
+        print("do things")
+    }else if name == 10{
+        print("do other things")
+    }else{
+        print(name)
+    }
 
-var error = 404
-// # https://shiftwebsolutions.com/15-common-web-errors/
-// # do more here later
-switch error {
-   case 404 :
-      print(" ")
-      fallthrough
-      // # look what this does later
-   case 500 :
-      print(" ")
-   case 401 :
-      print(" ")
-   default :
-      print( "")
+    var error = 404
+    // https://shiftwebsolutions.com/15-common-web-errors/
+    // do more here later
+    switch error {
+       case 404 :
+          print(" ")
+          fallthrough
+          // # look what this does later
+       case 500 :
+          print(" ")
+       case 401 :
+          print(" ")
+       default :
+          print( "")
+    }
+
 }
--- swift/swift classes/swift classes/syntax.swift
+++
@@ -1,9 +0,0 @@
-//
-//  syntax.swift
-//  swift classes
-//
-//  Created by Maks Drzezdzon on 12/11/2019.
-//  Copyright © 2019 Maks Drzezdzon. All rights reserved.
-//
-
-import Foundation
