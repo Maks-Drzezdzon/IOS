d2cd57aa7a399308312940130b01eddb87d39840
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-11-23 20:28:43 +0000

    done with interviews for now, amazong tomorrow some swift and chill today

--- swift/swift classes/swift classes/main.swift
+++ swift/swift classes/swift classes/main.swift
@@ -8,3 +8,51 @@ print(superMaks.move())
 print(superMaks.Admin())
 
 
+//  delegate pattern
+//  this is an interface in java
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituiation() {
+        print("does the thing")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+    
+}
+
+//structs cant inherit but can implement protocols/interfaces
+struct Paramdeic: AdvancedLifeSupport {
+    func performCPR() {
+        print("do the paramedic thing")
+    }
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+}
+
+class Doctor: AdvancedLifeSupport{
+    func performCPR() {
+        print("do doctor cpr thing")
+    }
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    
+}
+
+let e = EmergencyCallHandler()
+let p = Paramdeic(handler: <#T##EmergencyCallHandler#>)
+
+
+
