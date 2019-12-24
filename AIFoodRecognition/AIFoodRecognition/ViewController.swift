c28f28d1622b68006ec6573f71886dc1b68a2ed8
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-24 15:44:38 +0000

    changing from swiftUI to storyboard for now

---
+++ AIFoodRecognition/AIFoodRecognition/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
