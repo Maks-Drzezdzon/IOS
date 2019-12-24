1205ac24d35846ba1c18a43e1c6486f4dad8bcd9
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-24 16:02:29 +0000

    done permissions, and UI layout, need to go find old iphone

--- AIFoodRecognition/AIFoodRecognition/ViewController.swift
+++ AIFoodRecognition/AIFoodRecognition/ViewController.swift
@@ -5,12 +5,33 @@ import Vision
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // implementing camre
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        //imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
     }
-
+    
+    // picked image and do something with it
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // info is a dict
+        // you dont know what data type the data is so you optional bind it to uiimage
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
 }
