df4979c41abec2cfb6c6172f00653b2ab97394f7
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-24 16:14:00 +0000

    linking camera with model and passing picture

--- AIFoodRecognition/AIFoodRecognition/ViewController.swift
+++ AIFoodRecognition/AIFoodRecognition/ViewController.swift
@@ -24,11 +24,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // you dont know what data type the data is so you optional bind it to uiimage
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             imageView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("could not convert to ci image")
+            }
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("loading core ml model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("model failed to proccess image")
+            }
+        print(results)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+    }
+    
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
