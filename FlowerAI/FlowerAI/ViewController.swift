e9fc8c83464987b12e7f9e79cc987468156f05d2
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-25 18:50:26 +0000

    setting up camera

--- FlowerAI/FlowerAI/ViewController.swift
+++ FlowerAI/FlowerAI/ViewController.swift
@@ -1,12 +1,20 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imgView: UIImageView!
+    
+    let iamgePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        iamgePicker.delegate = self
     }
 
-
+    
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
