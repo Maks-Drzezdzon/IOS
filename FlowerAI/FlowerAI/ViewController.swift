9b5d8e59aa758be02a2365d3e2f41c49802b81c5
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2019-12-25 19:04:16 +0000

    setting up app code and hooking up ml model

--- FlowerAI/FlowerAI/ViewController.swift
+++ FlowerAI/FlowerAI/ViewController.swift
@@ -1,19 +1,63 @@
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imgView: UIImageView!
     
-    let iamgePicker = UIImagePickerController()
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        iamgePicker.delegate = self
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
     }
 
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imgView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("could not convert to ci image")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
     
     @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError("loading core ml model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("model failed to proccess image")
+            }
+            
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("flower"){
+                    self.navigationItem.title = "Flower!"
+                }else{
+                    self.navigationItem.title = firstResult.identifier
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
     }
     
 }
