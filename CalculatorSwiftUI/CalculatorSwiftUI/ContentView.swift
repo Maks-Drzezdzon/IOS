8e391c7934663277e36a069e31fe9ed990593b34
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-01-04 18:45:49 +0000

    some swiftUI inbetween interviews

--- CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["1", "2", "3" , "4"],
+        ["1", "2", "3" , "4"],
+        ["1", "2", "3" , "4"],
+        ["1", "2", "3" , "4"]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color.black
+            VStack{
+                ForEach(buttons, id : \.self){ row in
+                    HStack{
+                        ForEach(row, id: \.self){ button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                
+            }
+        }
     }
 }
 
