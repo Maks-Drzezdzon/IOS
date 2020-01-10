69118dafea2bc2322ae89d59309025082163fee9
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-01-10 17:49:36 +0000

    more of the same

--- WorldTemp/WorldTemp/ViewController.swift
+++ WorldTemp/WorldTemp/ViewController.swift
@@ -6,10 +6,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // origin x and y to draw out UI
+        // these are not pixels they are points which stay relative across all
+        // devices
         let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
         let firstView = UIView(frame: firstFrame)
         firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
+        // view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.blue
+        firstView.addSubview(secondView)
         
         
     }
