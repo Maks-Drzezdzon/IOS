0ffcae87e274a72067b9b53a2783aeff7408287a
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-01-10 17:50:11 +0000

    didnt change color of second drawable

--- WorldTemp/WorldTemp/ViewController.swift
+++ WorldTemp/WorldTemp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         
         let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
         let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.blue
+        secondView.backgroundColor = UIColor.green
         firstView.addSubview(secondView)
         
         
