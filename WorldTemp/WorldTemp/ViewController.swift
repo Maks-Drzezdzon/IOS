3cf775edcaa76a220ca3d7c433508c7a8c9676c1
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-01-10 21:01:45 +0000

    moving around in autolayout

--- WorldTemp/WorldTemp/ViewController.swift
+++ WorldTemp/WorldTemp/ViewController.swift
@@ -4,21 +4,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // origin x and y to draw out UI
-        // these are not pixels they are points which stay relative across all
-        // devices
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        // view.addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.green
-        firstView.addSubview(secondView)
-        
-        
     }
     
 }
