ce6356cb7d751df9a396b9ef2fdb3e0912b1df22
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-01-14 23:10:38 +0000

    moving onto gestures

--- WorldTemp/WorldTemp/ConcersionViewController.swift
+++ WorldTemp/WorldTemp/ConcersionViewController.swift
@@ -3,17 +3,21 @@ import UIKit
 class ConcversionViewController: UIViewController {
     
     @IBOutlet weak var celsiousLabel: UITextField!
-    
+    @IBOutlet weak var textField: UILabel!
     
     @IBAction func fahrenheitFieldEditingChnaged(_ textField: UITextField) {
         // connected this method to controller via sent events
-        // to update dynamically without sending like angular 
+        // to update dynamically without sending like angular
         
         if let text = textField.text, !text.isEmpty{
             celsiousLabel.text = text
         }else{
             celsiousLabel.text = "???"
         }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
+        textField.resignFirstResponder()
     }
     
 }
