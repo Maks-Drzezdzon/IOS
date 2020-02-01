c693e397cfe0033f76c79b87f0b1ef95b76f85c5
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-02-01 20:35:04 +0000

    drawing blocks

--- TetrisSwiftUI/TetrisSwiftUI/View/TetrisGameView.swift
+++ TetrisSwiftUI/TetrisSwiftUI/View/TetrisGameView.swift
@@ -4,7 +4,27 @@ struct TetrisGameView: View{
     @ObservedObject var tetrisGame = TetrisGameViewModel()
     
     var body: some View{
-        Text("Test")
+        GeometryReader{(geometry: GeometryProxy) in
+            self.drawBoard(boundingReact: geometry.size)
+        }
+    }
+    
+    func drawBoard(boundingReact: CGSize) -> some View {
+        let columns = self.tetrisGame.numColumns
+        let rows = self.tetrisGame.numRows
+        // scaling to screen size
+        let blockSize = min(boundingReact.width/CGFloat(columns), boundingReact.height/CGFloat(rows))
+        let yoffset = (boundingReact.width - blockSize*CGFloat(columns))/2
+        let xoffset = (boundingReact.height - blockSize*CGFloat(rows))/2
+        
+        return ForEach(0...columns-1, id:\.self){ (column: Int) in
+            ForEach(0...rows-1, id:\.self){ (row: Int) in
+                let x = xoffset + blockSize * CGFloat(column)
+                let y = boundingReact.height - yoffset - blockSize*CGFloat(rows - 1)
+            }
+            
+        }
+        
     }
 }
 
