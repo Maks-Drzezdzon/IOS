4b9c4bbaf5a2e9c301b61bf975ec25e1eca270c5
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-02-02 20:17:13 +0000

    need to restruct project

--- TetrisSwiftUI/TetrisSwiftUI/View/TetrisGameView.swift
+++ TetrisSwiftUI/TetrisSwiftUI/View/TetrisGameView.swift
@@ -14,13 +14,19 @@ struct TetrisGameView: View{
         let rows = self.tetrisGame.numRows
         // scaling to screen size
         let blockSize = min(boundingReact.width/CGFloat(columns), boundingReact.height/CGFloat(rows))
-        let yoffset = (boundingReact.width - blockSize*CGFloat(columns))/2
-        let xoffset = (boundingReact.height - blockSize*CGFloat(rows))/2
+        let xoffset = (boundingReact.width - blockSize*CGFloat(columns))/2
+        let yoffset = (boundingReact.height - blockSize*CGFloat(rows))/2
         
         return ForEach(0...columns-1, id:\.self){ (column: Int) in
             ForEach(0...rows-1, id:\.self){ (row: Int) in
-                let x = xoffset + blockSize * CGFloat(column)
-                let y = boundingReact.height - yoffset - blockSize*CGFloat(rows - 1)
+                Path{ path in
+                    let x = xoffset + blockSize * CGFloat(column)
+                    let y = boundingReact.height - yoffset - blockSize*CGFloat(rows - 1)
+                    
+                    let rect = CGRect(x: x, y: y, width: blockSize, height: blockSize)
+                    path.addRect(rect)
+                }
+                .fill(self.tetrisGame.gameBoard[column][row].color)
             }
             
         }
