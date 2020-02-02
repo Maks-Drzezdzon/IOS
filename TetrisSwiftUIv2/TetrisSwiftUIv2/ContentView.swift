1df4b3d457ddd4decdefda3afbc245e4cdb6dd11
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-02-02 20:18:01 +0000

    moving over

---
+++ TetrisSwiftUIv2/TetrisSwiftUIv2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TetrisSwiftUIv2
+//
+//  Created by Maks Drzezdzon on 02/02/2020.
+//  Copyright © 2020 Maks Drzezdzon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
