da8d4b23aae03c68c380925ed56a4fc913d66d70
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-02-03 20:50:22 +0000

    adding colors

--- TetrisSwiftUIv2/TetrisSwiftUIv2/Extension/Color.swift
+++ TetrisSwiftUIv2/TetrisSwiftUIv2/Extension/Color.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 extension Color {
-    public static var tetrisBlack = Color(red: 0/255, green: 0/255, blue: 0/255)
+    public static var tetrisOrange = Color(red: 255/255, green: 115/255, blue: 8/255)
+    public static var tetrisGreen = Color(red: 255/255, green: 241/255, blue: 60/255)
+    public static var tetrisLightBlue = Color(red: 0/255, green: 230/255, blue: 254/255)
+    public static var tetrisDarkBlue = Color(red: 1/255, green: 1/255, blue: 240/255)
+    public static var tetrisYellow = Color(red: 255/255, green: 222/255, blue: 0/255)
     public static var tetrisRed = Color(red: 255/255, green: 16/255, blue: 60/255)
+    public static var tetrisPurple = Color(red: 184/255, green: 2/255, blue: 253/255)
+    public static var tetrisBlack = Color(red: 0/255, green: 0/255, blue: 0/255)
 }
