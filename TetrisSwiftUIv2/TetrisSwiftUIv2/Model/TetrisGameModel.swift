b7bcfb1780329a4fb5c00c2a5a69d3344d89d7fa
Maksymilian <38735814+Maksthorn@users.noreply.github.com>
2020-02-03 20:56:15 +0000

    adding blocks

---
+++ TetrisSwiftUIv2/TetrisSwiftUIv2/Model/TetrisGameModel.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+
+class TetrisGameModel: ObservableObject {
+    // observe changes so view will update automatically
+    var numRows: Int
+    var numColumns: Int
+    @Published var gameBoard: [[TetrisGmaeBlock?]]
+    
+    init(numRows: Int = 23, numColumns: Int = 10) {
+        self.numRows = numRows
+        self.numColumns = numColumns
+        
+        gameBoard = Array(repeating: Array(repeating: TetrisGameSquare(color: Color.tetrisBlack), count: numRows), count: numColumns)
+    }
+}
+
+struct TetrisGmaeBlock {
+    var blockType: BlockType
+}
+
+enum BlockType{
+    case i, t, o, j, l, s, z
+}
